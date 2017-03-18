d7726f17fdb040bde5b608191b30726be99bc2d4
Mihovil <mihovil.kovacevic@shoutem.com>
2017-03-18 20:09:36 +0100

    Add Facebook login

--- shoutem-auth/app/middleware.js
+++ shoutem-auth/app/middleware.js
@@ -5,19 +5,19 @@ import URI from 'urijs';
 import { redirectTo, NAVIGATE, NavigationOperations } from '@shoutem/core/navigation';
 import { priorities, setPriority, before } from '@shoutem/core/middlewareUtils';
 import { RESTART_APP } from '@shoutem/core/coreRedux';
-import { purgeStore } from 'shoutem.persist';
 import { getExtensionSettings } from 'shoutem.application';
 
 import { ext } from './const';
 import { isAuthenticated, LOGOUT, AUTHENTICATE } from './redux';
 import { getAuthHeader } from './shared/getAuthHeader';
 import { isAuthenticationRequired } from './isAuthenticationRequired';
+import { clearSession } from './session.js';
 
 const APPLICATION_EXTENSION = 'shoutem.application';
 const AUTH_HEADERS = 'headers.Authorization';
 
 export function createLoginMiddleware(screens) {
-  return setPriority(store => next => action => {
+  return setPriority(store => next => (action) => {
     if (action.type === NAVIGATE) {
       const state = store.getState();
       if (isAuthenticationRequired(screens, action, state) && !isAuthenticated(state)) {
@@ -38,7 +38,7 @@ export function createLoginMiddleware(screens) {
   }, priorities.AUTH);
 }
 
-export const authenticateMiddleware = setPriority(store => next => action => {
+export const authenticateMiddleware = setPriority(store => next => (action) => {
   if (action.type === AUTHENTICATE) {
     const state = store.getState();
 
@@ -64,35 +64,36 @@ export const authenticateMiddleware = setPriority(store => next => action => {
  * Sets header Authorization value for every network request to endpoints registered
  * in shoutem.application that doesn't already include any Authorization header
  */
-export function createNetworkRequestMiddleware() {
-  return setPriority(store => next => action => {
-    if (isRSAA(action)) {
-      const state = store.getState();
-      const appSettings = getExtensionSettings(state, APPLICATION_EXTENSION);
-      const servers = _.reduce([appSettings.legacyApiEndpoint, appSettings.apiEndpoint], (result, server) => {
-        if (server) {
-          result.push(new URI(server).hostname());
-        }
+export const networkRequestMiddleware = setPriority(store => next => (action) => {
+  if (isRSAA(action)) {
+    const state = store.getState();
+    const appSettings = getExtensionSettings(state, APPLICATION_EXTENSION);
 
-        return result;
-      }, []);
-      const endpoint = new URI(action[RSAA].endpoint).hostname();
-      if (servers.includes(endpoint) && !_.has(action[RSAA], AUTH_HEADERS)) {
-        _.set(action[RSAA], AUTH_HEADERS, getAuthHeader(state));
+    const { legacyApiEndpoint, apiEndpoint } = appSettings;
+    const servers = _.reduce([legacyApiEndpoint, apiEndpoint], (result, server) => {
+      if (server) {
+        result.push(new URI(server).hostname());
       }
+
+      return result;
+    }, []);
+    const endpoint = new URI(action[RSAA].endpoint).hostname();
+    if (servers.includes(endpoint) && !_.has(action[RSAA], AUTH_HEADERS)) {
+      _.set(action[RSAA], AUTH_HEADERS, getAuthHeader(state));
     }
+  }
 
-    return next(action);
-  }, before(priorities.NETWORKING));
-}
+  return next(action);
+}, before(priorities.NETWORKING));
 
-export const logoutMiddleware = setPriority(store => next => action => {
+export const logoutMiddleware = setPriority(store => next => (action) => {
   const actionType = _.get(action, 'type');
 
   if (actionType === LOGOUT) {
-    purgeStore().then(
+    clearSession().then(
       () => store.dispatch({ type: RESTART_APP }),
-      (reason) => console.warn(reason));
+      reason => console.warn(reason),
+    );
   }
   return next(action);
 }, priorities.AUTH);
