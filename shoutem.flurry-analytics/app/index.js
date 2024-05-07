429a54bbf40c9089a28416512ded6d3070622d73
tomislav-arambasic <tomislav.arambasic@gmail.com>
2024-05-07 12:22:45 +0200

    remove deprecated flurry-analytics extension

--- shoutem.flurry-analytics/app/index.js
+++
@@ -1,7 +0,0 @@
-// Reference for app/index.js can be found here:
-// http://shoutem.github.io/docs/extensions/reference/extension-exports
-
-import { appDidMount } from './app';
-import middleware from './middleware';
-
-export { appDidMount, middleware };
--- shoutem.flurry-analytics/app/permissions.js
+++
@@ -1,9 +0,0 @@
-const {
-  PERMISSION_TYPES,
-} = require('../../shoutem.permissions/app/build/const');
-
-const permissions = [
-  { type: PERMISSION_TYPES.ANDROID_ACCESS_NETWORK_STATE }
-];
-
-module.exports = { permissions };
