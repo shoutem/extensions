09e875bfa65d4a03e03d3de0957bf2b326e4db2c
tomislav-arambasic <tomislav.arambasic@gmail.com>
2024-11-21 15:19:21 +0100

    updates as of extensions 15.10

--- shoutem.web-view/app/screens/WebViewWithShareScreen.js
+++ shoutem.web-view/app/screens/WebViewWithShareScreen.js
@@ -4,13 +4,24 @@ import { ShareButton } from '@shoutem/ui';
 import { currentLocation } from 'shoutem.cms';
 import { ext } from '../const';
 import { WebViewScreen } from './WebViewScreen';
+import { connect } from 'react-redux';
+import { mapDispatchToProps } from './WebViewScreen';
+import { getRouteParams } from 'shoutem.navigation';
 
 export class WebViewWithShareScreen extends WebViewScreen {
   getNavBarProps() {
     const { title = '', url } = this.getSettings();
 
+    // showScreenTitle was implemented recently and default value is true.
+    // We should only hide header title if app owner explicitly disabled the option
+    // and republished the app.
+    const resolvedTitle =
+      getRouteParams(this.props).shortcut.settings.showScreenTitle === false
+        ? ''
+        : title;
+
     return {
-      title,
+      title: resolvedTitle,
       headerRight: headerProps => (
         <ShareButton
           iconProps={{ style: headerProps.tintColor }}
@@ -31,6 +42,11 @@ WebViewWithShareScreen.defaultProps = {
   ...WebViewScreen.defaultProps,
 };
 
-export default connectStyle(ext('WebViewWithShareScreen'))(
-  currentLocation(WebViewWithShareScreen),
+export default connect(
+  null,
+  mapDispatchToProps,
+)(
+  connectStyle(ext('WebViewWithShareScreen'))(
+    currentLocation(WebViewWithShareScreen),
+  ),
 );
